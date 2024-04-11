b32b9d72f781b32a1191be88ddfb91e57a3ee293
Thanh Sơn <hogiangthanhson@gmail.com>
2024-04-11 15:12:48 +0700

    feat(PackingList): build PackingList

--- src/component/PackingList.tsx
+++ src/component/PackingList.tsx
@@ -1,3 +1,26 @@
+import Item from "./Item";
+
+export interface Items {
+  id: number;
+  description: string;
+  quantity: number;
+  packed: boolean;
+}
+
+const initialItems: Items[] = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
