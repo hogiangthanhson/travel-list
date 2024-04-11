6726a61c23af421c719060eeea8f6946de3cec75
Thanh Sơn <hogiangthanhson@gmail.com>
2024-04-11 14:22:26 +0700

    feat(component): build component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from "./component/Form";
+import Logo from "./component/Logo";
+import PackingList from "./component/PackingList";
+import Stats from "./component/Stats";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
-
-export default App;
---
+++ src/component/Form.tsx
@@ -0,0 +1,10 @@
+export default function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip</h3>
+      <button></button>
+      <select name="" id=""></select>
+      <input type="text" />
+    </div>
+  );
+}
