a2cd543a06e3201af16e55e9bafe0521286318b5
Thanh Sơn <hogiangthanhson@gmail.com>
2024-04-11 17:00:56 +0700

    feat(Logo): build Logo

--- src/component/Form.tsx
+++ src/component/Form.tsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip</h3>
-      <button></button>
-      <select name="" id=""></select>
-      <input type="text" />
-    </div>
+      <select name="" id="" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((number) => (
+          <option value={number} key={number}>
+            {number}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>ADD</button>
+    </form>
   );
 }
